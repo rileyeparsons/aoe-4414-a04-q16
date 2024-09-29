eddb32452cf93f1580ddde19a1a792a3459c96fd
Riley Parsons <rileyep@vt.edu>
2024-09-29 14:07:09 -0400

    ecef_to_sez, finished

--- ecef_to_sez.py
+++ ecef_to_sez.py
@@ -28,22 +28,77 @@
 # Written by Riley Parsons
 
 import sys
+import math
 
 # "constants"
+R_E_KM = 6378.137
+E_E    = 0.081819221456
 
 # helper functions  
+def calc_denom(ecc, lat_rad):
+  return math.sqrt(1.0-(ecc**2)*(math.sin(lat_rad)**2))
+
+def ecef_to_llh(r_x_km, r_y_km, r_z_km):
+ # calculate longitude
+ lon_rad = math.atan2(r_y_km,r_x_km)
+ lon_deg = lon_rad*180.0/math.pi
+ 
+ # initialize lat_rad, r_lon_km, r_z_km
+ lat_rad = math.asin(r_z_km/math.sqrt(r_x_km**2+r_y_km**2+r_z_km**2))
+ r_lon_km = math.sqrt(r_x_km**2+r_y_km**2)
+ prev_lat_rad = float('nan')
+ 
+ # iteratively find latitude
+ c_E = float('nan')
+ count = 0
+ while (math.isnan(prev_lat_rad) or abs(lat_rad-prev_lat_rad)>10e-7) and count<5:
+   denom = calc_denom(E_E,lat_rad)
+   c_E = R_E_KM/denom
+   prev_lat_rad = lat_rad
+   lat_rad = math.atan((r_z_km+c_E*(E_E**2)*math.sin(lat_rad))/r_lon_km)
+   count = count+1
+   
+ # calculate hae
+ hae_km = r_lon_km/math.cos(lat_rad)-c_E
+
+ return [lon_deg, lat_rad*180.0/math.pi, hae_km]
+
+def simple_rot(three_three_mat, three_one_mat):
+  """
+  no numpy :(
+  """
+  r1 = three_three_mat[0][0]*three_one_mat[0] + three_three_mat[0][1]*three_one_mat[1] + three_three_mat[0][2]*three_one_mat[2]
+  r2 = three_three_mat[1][0]*three_one_mat[0] + three_three_mat[1][1]*three_one_mat[1] + three_three_mat[1][2]*three_one_mat[2]
+  r3 = three_three_mat[2][0]*three_one_mat[0] + three_three_mat[2][1]*three_one_mat[1] + three_three_mat[2][2]*three_one_mat[2]
+  return [r1, r2, r3]
 
 # main function
 def ecef_to_sez(o_x_km, o_y_km, o_z_km, x_km, y_km, z_km):
-  r_s = o_x_km + x_km
-  r_e = o_y_km + y_km
-  r_z = o_z_km + z_km
+  ecef_x_km = x_km - o_x_km
+  ecef_y_km = y_km - o_y_km 
+  ecef_z_km = z_km - o_z_km
 
-  print(round(r_s,3))
-  print(round(r_e, 3))
-  print(round(r_z, 3))
+  llh = ecef_to_llh(o_x_km, o_y_km, o_z_km)
+  
+  # not putting this part in a function dont feel like it
+  Rz_inv = [
+    [math.sin(llh[1]*(math.pi/180)), 0, -math.cos(llh[1]*(math.pi/180))], 
+    [0, 1, 0], 
+    [math.cos(llh[1]*(math.pi/180)), 0, math.sin(llh[1]*(math.pi/180))]
+  ]
 
-  return [r_s, r_e, r_z]
+  Ry_inv = [
+    [math.cos((llh[0])*(math.pi/180)), math.sin((llh[0])*(math.pi/180)), 0], 
+    [-math.sin((llh[0])*(math.pi/180)), math.cos((llh[0])*(math.pi/180)), 0],
+    [0, 0, 1]
+  ]
+
+  sez = simple_rot(Rz_inv, simple_rot(Ry_inv, [ecef_x_km, ecef_y_km, ecef_z_km]))
+  print(sez[0])
+  print(sez[1])
+  print(sez[2])
+
+  return sez
   
 # initialize script arguments
 o_x_km = None
